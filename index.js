e29f7cbf4af2b87a4c3c6390c755028eaa9d013b
Allie Zhao <allie.k.zhao@gmail.com>
2023-01-10 12:56:30 -0800

    did all the exercises

--- index.js
+++ index.js
@@ -3,26 +3,38 @@
 // OUTPUT: the value of the 'name' attribute i.e. Benny
 // REQS: use destructuring and the function should be a single line
 
-export const getName = () => {}
+export const getName = ({ name }) => {
+  return name
+}
 
 // INPUT: an object with a nested "address" attribute such as
 //   {name: 'Bob Smith', address: {street: 'Main Street', number: 123, city: 'Anytown', country: 'USA}}
 // OUTPUT: the string 'Bob Smith lives at 123 Main Street, Anytown, USA'
 // REQS: use destructuring and template literals
 
-export const printAddress = () => {}
+export const printAddress = ({ name, address }) => {
+  return `${name} lives at ${address.number} ${address.street}, ${address.city}, ${address.country}`
+}
 
 // REFACTOR CHALLENGE
 // Refactor this function so that all values in the object are destructured
 // as part of the funciton definitions (i.e. there should be no dots in the template literals)
-export const printUserInfo = (user) => {
+export const printUserInfo = ({
+  username,
+  name: { first, last },
+  info: {
+    favorites: { color, food },
+    pet: { name },
+    address: { street, number, city, country },
+  },
+}) => {
   return `
-    Username: ${user.username},
-    Full Name: ${user.name.first} ${user.name.last},
-    Favorite Color: ${user.info.favorites.color},
-    Favorite Food: ${user.info.favorites.food},
-    Pet Name: ${user.info.pet.name},
-    Address: ${user.info.address.number} ${user.info.address.street}, ${user.info.address.city}, ${user.info.address.country}
+    Username: ${username},
+    Full Name: ${first} ${last},
+    Favorite Color: ${color},
+    Favorite Food: ${food},
+    Pet Name: ${name},
+    Address: ${number} ${street}, ${city}, ${country}
     `
 }
 
@@ -31,14 +43,23 @@ export const printUserInfo = (user) => {
 // REQS: use rest parameters
 //  getSum(1, 2, 3) === 6
 //  getSum(1, 2, 3, 4, 5) === 15
-export const getSum = () => {}
+export const getSum = (...rest) => {
+  let total = 0
+  for (let num of rest) {
+    total = total + num
+  }
+
+  return total
+}
 
 // INPUT: an unknown number of arguments
 // OUTPUT: an array with the first two arguments destructured and the remaining in a nested array
 // REQS: use rest parameters
 // getFirstTwoArgs(1, 2, 3, 4, 5) should return [1, 2, [3, 4, 5]]
 // getFirstTwoArgs('a', 'b', 'c', 'd') should return ['a', 'b', ['c', 'd']]
-export const getFirstTwoArgs = () => {}
+export const getFirstTwoArgs = (a, b, ...remainder) => {
+  return [a, b, remainder]
+}
 
 // INPUT: an object with the following structure
 // {
@@ -61,16 +82,30 @@ export const getFirstTwoArgs = () => {}
 //    return a NEW object, do not modify the object passed in to the function
 //    use spread operator to create a new object
 
-export const addSneakerCount = () => {}
+export const addSneakerCount = ({ shoes, slogan, logo, headquarters }) => {
+  const shoeList = [...shoes]
+  const sneakerCount = shoeList.length
+
+  const nObj = { shoeList, slogan, logo, headquarters, sneakerCount }
+  return nObj
+}
 
 // INPUT: brands from data.js
 // OUTPUT: the brand names listed
 // REQS: use Object.keys to solve
-export const getBrandNames = () => {}
+export const getBrandNames = (brands) => {
+  return Object.keys(brands)
+}
 
 // INPUT: brands from data.js
 // OUTPUT: total number of sneaker types across all brands (14)
-export const totalSneakerCount = () => {}
+export const totalSneakerCount = (brands) => {
+  const sneakers = []
+  for (const brand of Object.keys(brands)) {
+    sneakers.push(...brands[brand].shoes)
+  }
+  return sneakers.length
+}
 
 // INPUT: An object
 // OUTPUT: An array with key value pairs converted to arrays
@@ -79,6 +114,13 @@ export const totalSneakerCount = () => {}
 // convertToArray({}) => []
 // Source: https://edabit.com/challenge/pPNAs5PvB3WvnDwDM
 
-export const convertToArray = () => {}
+export const convertToArray = (obj) => {
+  const arr = []
+  for (const [key, value] of Object.entries(obj)) {
+    const pair = [key, value]
+    arr.push(pair)
+  }
+  return arr
+}
 
 //
--- test/index.test.js
+++ test/index.test.js
@@ -15,7 +15,7 @@ test('getName should return the name attribute from an object', () => {
   expect(getName({ name: 'Benny' })).toBe('Benny')
 })
 
-test.skip('printAddress should return a formatted string', () => {
+test('printAddress should return a formatted string', () => {
   const input = {
     name: 'Bob Smith',
     address: { street: 'Main Street', number: 123, city: 'Anytown', country: 'USA' },
@@ -23,7 +23,7 @@ test.skip('printAddress should return a formatted string', () => {
   expect(printAddress(input)).toBe('Bob Smith lives at 123 Main Street, Anytown, USA')
 })
 
-test.skip('printUserInfo should return a formatted string', () => {
+test('printUserInfo should return a formatted string', () => {
   const user = {
     username: 'bobloblaw',
     name: {
@@ -46,17 +46,17 @@ test.skip('printUserInfo should return a formatted string', () => {
     `)
 })
 
-test.skip('getSum should return the sum of inputs', () => {
+test('getSum should return the sum of inputs', () => {
   expect(getSum(1, 2, 3)).toBe(6)
   expect(getSum(1, 2, 3, 4, 5)).toBe(15)
 })
 
-test.skip('getFirstTwoArgs returns the first two arguments as an array', () => {
+test('getFirstTwoArgs returns the first two arguments as an array', () => {
   expect(getFirstTwoArgs(1, 2, 3, 4, 5)).toStrictEqual([1, 2, [3, 4, 5]])
   expect(getFirstTwoArgs('a', 'b', 'c', 'd')).toStrictEqual(['a', 'b', ['c', 'd']])
 })
 
-test.skip('addSneakerCount should append the sneaker count to the object', () => {
+test('addSneakerCount should append the sneaker count to the object', () => {
   const nike = {
     shoes: ['Air Max', 'Air Force', 'Air Jordan', 'Air Mag', 'Pegasus', 'Kobe'],
     slogan: 'Just Do It',
@@ -67,15 +67,15 @@ test.skip('addSneakerCount should append the sneaker count to the object', () =>
   expect(addSneakerCount(nike)).not.toBe(nike)
 })
 
-test.skip('getBrandNames should return an array of the names of the sneaker brands', () => {
+test('getBrandNames should return an array of the names of the sneaker brands', () => {
   expect(getBrandNames(brands)).toStrictEqual(['Nike', 'Puma', 'Adidas'])
 })
 
-test.skip('totalSneakerCount should return total number of sneakers', () => {
+test('totalSneakerCount should return total number of sneakers', () => {
   expect(totalSneakerCount(brands)).toBe(14)
 })
 
-test.skip('convertToArray should convert Object key, value pairs to arrays', () => {
+test('convertToArray should convert Object key, value pairs to arrays', () => {
   expect(convertToArray({ a: 1, b: 2, c: 3 })).toStrictEqual([
     ['a', 1],
     ['b', 2],
